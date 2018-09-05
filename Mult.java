0390977adc008e27ddadf481a9c967abf8298689
Demmyzzz <33146623+Demmyzzz@users.noreply.github.com>
2018-09-05 12:52:17 +0300

    Add files via upload

---
+++ Mult.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Mult {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("1:");
+        int numOne = sc.nextInt();
+        System.out.print("2:");
+        int numTwo = sc.nextInt();
+        int res = um(numOne, numTwo);
+        System.out.println(res + " - result");
+    }
+    private static int um(int numOne, int numTwo) {
+        int result = 0;
+        if (numOne < 0 && numTwo < 0){numOne=-numOne; numTwo=-numTwo;}
+        if (numOne>numTwo) { int s;s = numTwo;numTwo = numOne;numOne = s; }
+        if (numTwo<0){ numTwo=-numTwo; }
+        if (numOne == 0 || numTwo == 0){ return result; } else{ for (int i=0;i<numTwo; i++){ result += numOne; }return result; }
+    }
+}
